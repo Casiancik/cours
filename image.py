fc58d7224fa721bc66c6d8b1da2b216867309504
Casiancik <108890705+Casiancik@users.noreply.github.com>
2025-12-22 14:53:09 +0100

    Add files via upload

--- image.py
+++ image.py
@@ -3,9 +3,13 @@ from PIL import Image
 img = Image.open("test.jpg")
 red, green, blue = img.split()
 
-new_red = red.crop((100, 0, red.width, red.height))
-new_green = green.crop((0, 0, green.width - 100, green.height))
-new_blue = blue.crop((50, 0, blue.width - 50, blue.height))
+one_red = red.crop((100, 0, red.width, red.height))
+second_red = red.crop((50, 0, red.width - 50, red.height))
+new_red = Image.blend(one_red, second_red, 0.5)
+new_green = green.crop((50, 0, green.width - 50, green.height))
+one_blue = blue.crop((0, 0, blue.width - 100, blue.height))
+second_blue = blue.crop((50, 0, blue.width - 50, blue.height))
+new_blue = Image.blend(one_blue, second_blue, 0.5)
 
 
 new_image = Image.merge("RGB", (new_red, new_green, new_blue))
