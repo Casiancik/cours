6fe13dd48ebaf704c809b93767f570781909bca3
Casiancik <108890705+Casiancik@users.noreply.github.com>
2025-12-22 13:11:26 +0100

    Add files via upload

--- image.py
+++ image.py
@@ -4,8 +4,8 @@ img = Image.open("test.jpg")
 red, green, blue = img.split()
 
 new_red = red.crop((100, 0, red.width, red.height))
-new_green = green.crop((0, 0, blue.width - 100, blue.height))
-new_blue = blue.crop((50, 0, green.width - 50, green.height))
+new_green = green.crop((0, 0, green.width - 100, green.height))
+new_blue = blue.crop((50, 0, blue.width - 50, blue.height))
 
 
 new_image = Image.merge("RGB", (new_red, new_green, new_blue))
