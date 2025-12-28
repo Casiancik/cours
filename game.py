20792fcb0686294250426ea7e9e8a74ee247e753
Casiancik <108890705+Casiancik@users.noreply.github.com>
2025-12-28 15:00:24 +0100

    Add files via upload

---
+++ game.py
@@ -0,0 +1,97 @@
+import file_operations
+from faker import Faker
+import random
+import os
+
+
+RUNIC_LETTERS = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е͠',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋'
+}
+
+
+SKILLS = [
+    "Стремительный прыжок",
+    "Электрический выстрел",
+    "Ледяной удар",
+    "Стремительный удар",
+    "Кислотный взгляд",
+    "Тайный побег",
+    "Ледяной выстрел",
+    "Огненный заряд",
+]
+
+
+def get_runic_skill():
+    word = ""
+    runic_skill = []
+    for word in SKILLS:
+        new_word = word
+        for character in word:
+            runic_character = RUNIC_LETTERS.get(character, character)
+            new_word = new_word.replace(character, runic_character)
+        runic_skill.append(new_word)
+    return runic_skill
+
+
+def user_generate():
+    fake = Faker("ru_RU")
+    skill_1, skill_2, skill_3 = random.sample(get_runic_skill(), 3)
+    context = {
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "job": fake.job(),
+        "town": fake.city(),
+        "strength": random.randint(3, 18),
+        "agility": random.randint(3, 18),
+        "endurance": random.randint(3, 18),
+        "intelligence": random.randint(3, 18),
+        "luck": random.randint(3, 18),
+        "skill_1": skill_1,
+        "skill_2": skill_2,
+        "skill_3": skill_3, 
+    }
+    return context
+
+
+def generate_cards():
+    new_path = f"game"
+    os.makedirs(new_path, exist_ok=True)
+    new_name = ""
+    for new_name in range(1, 11):
+        card = file_operations.render_template("game.svg", "{0}/new_game_{1}.svg".format(new_path, new_name), user_generate())
+    return card
+
+
+def main():
+    generate_cards()
+
+if __name__ == "__main__":
+    main() 
+
+
+
+
+
+
